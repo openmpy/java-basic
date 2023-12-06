b26a98dcb4ec309f21ac8f357329b93f81bdd3c5
openmpy <suhwan@kakao.com>
2023-12-06 19:16:14 +0900

    생성자

---
+++ src/construct/ex/Book.java
@@ -0,0 +1,27 @@
+package construct.ex;
+
+public class Book {
+    String title;
+    String author;
+    int page;
+
+    public Book() {
+        title = "";
+        author = "";
+    }
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(String title, String author, int page) {
+        this.title = title;
+        this.author = author;
+        this.page = page;
+    }
+
+    public void displayInfo() {
+        System.out.println("제목: " + title + ", 저자: " + author + ", 페이지: " + page);
+    }
+}
