4310c8115c88bc963c9191a5f6977b01a9ffcadf
openmpy <suhwan@kakao.com>
2023-12-13 21:48:01 +0900

    final

---
+++ src/final1/FinalLocalMain.java
@@ -0,0 +1,12 @@
+package final1;
+
+public class FinalLocalMain {
+
+    public static void main(String[] args) {
+
+        // final 지역 변수1
+        final int data1;
+        data1 = 10; // 최초 한번만 할당 가능
+//        data1 = 20; // 컴파일 에러
+    }
+}
